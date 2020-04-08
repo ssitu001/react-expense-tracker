d9bb34e9c9e8a966b2b7aa3b874c3d0babe94612
Simon Situ <ssitu001@gmail.com>
2020-04-07 19:06:59 -0700

    update to types

--- src/types.ts
+++ src/types.ts
@@ -3,24 +3,35 @@ import {
   DELETE_TRANSACTION
 } from './actions/actions'
 
+export type IdType = string
+
 export interface Transaction {
-  id: number,
-  text: string,
-  amount: number,
+  id: IdType;
+  text: string;
+  amount: number;
 }
 
+export type Transactions = Transaction[]
+
 export interface State {
-  transactions: Transaction[]
+  transactions: Transactions
 }
 
 export interface AddTransactionAction {
-  type: typeof ADD_TRANSACTION,
-  payload: Transaction,
+  type: typeof ADD_TRANSACTION;
+  payload: Transaction;
 }
 
 export interface DeleteTransactionAction {
-  type: typeof DELETE_TRANSACTION,
-  payload: Transaction,
+  type: typeof DELETE_TRANSACTION;
+  payload: IdType;
 }
 
 export type TransactionActionTypes = AddTransactionAction | DeleteTransactionAction
+
+
+export interface ContextProps {
+  transactions: Transaction[];
+  deleteTransaction: (id: IdType) => void;
+  addTransaction: (transaction: Transaction) => void;
+}
