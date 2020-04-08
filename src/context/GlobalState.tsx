ac731ea7ff94d2868ea296fc02d0f1c8872982cd
Simon Situ <ssitu001@gmail.com>
2020-04-07 19:04:26 -0700

    globalstate mvo

--- src/context/GlobalState.tsx
+++ src/context/GlobalState.tsx
@@ -1,25 +1,55 @@
 import React, { createContext, useReducer } from 'react'
 import AppReducer from '../reducers/AppReducer'
 
+import {
+  IdType,
+  ContextProps,
+  Transaction,
+} from '../types'
+
+import {
+  ADD_TRANSACTION,
+  DELETE_TRANSACTION
+} from '../actions/actions'
+
 const dummyTransactions = [
-  { id: 1, text: 'Flower', amount: -20 },
-  { id: 2, text: 'Salary', amount: 300 },
-  { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: 150 }
+  { id: '1', text: 'Cheese', amount: -20 },
+  { id: '2', text: 'Pay day', amount: 3000 },
+  { id: '3', text: 'Burger', amount: -10 },
+  { id: '4', text: 'Macbook', amount: 1500 }
 ]
 
 const initialState = {
   transactions: [...dummyTransactions],
 }
 
-export const GlobalContext = createContext(initialState)
+export const GlobalContext = createContext<Partial<ContextProps>>(initialState)
 
 export const GlobalProvider: React.FC<{}> = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState)
+
+  // actions
+  const deleteTransaction = (id: IdType): void => {
+    dispatch({
+      type: DELETE_TRANSACTION,
+      payload: id,
+    })
+  }
+
+  const addTransaction = (transaction: Transaction): void => {
+    dispatch({
+      type: ADD_TRANSACTION,
+      payload: transaction,
+    })
+  }
   
   return (
     <GlobalContext.Provider
-      value={{ transactions: state.transactions }}>
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}>
       {children}
     </GlobalContext.Provider>
   )
