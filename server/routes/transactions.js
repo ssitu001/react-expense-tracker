fa8074a4afcb9306f169107ae56732183e5cd95b
Simon Situ <simonsitu@Simons-MacBook-Pro.local>
2020-04-11 20:37:25 -0700

    transaction routes

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => res.send('hello world yo'))
+const {
+    getTransactions,
+    addTransaction,
+    deleteTransaction,
+} = require('../controllers/transactionsController')
+
+router.route('/')
+    .get(getTransactions)
+    .post(addTransaction)
+
+router.route('/:id')
+    .delete(deleteTransaction)
 
 module.exports = router
